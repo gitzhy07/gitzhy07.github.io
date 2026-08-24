4ecfad0e5ade4b1fe0f74fcb73eba6bd0a9915e9
gitzhy07 <jimzhangya@163.com>
2026-08-24 10:21:41 +0800

    win

--- assets/js/main.js
+++ assets/js/main.js
@@ -50,7 +50,7 @@ $$('.rv, [data-scramble]').forEach(el => io.observe(el));
 
 /* ---------- 终端：打字引导 ---------- */
 const out  = $('#term-out'), body = $('#term-body'), input = $('#term-input');
-const PROMPT = 'guest@chenmo ~ % ';
+const PROMPT = 'PS C:\\Users\\guest\\blog> ';
 const SCRIPT = [
   {cmd:'whoami', out:['<b>z</b> · 后端工程师 / 分布式存储方向','写代码，也写关于代码的代码。']},
   {cmd:'cat ./now.txt', out:['正在攻坚 ▸ 用 <span class="hl">eBPF</span> 重写服务网格数据面','正在阅读 ▸ 《数据密集型应用系统设计》第 8 章']},
